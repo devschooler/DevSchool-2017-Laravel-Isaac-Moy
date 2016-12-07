67be64308442327a8c287a64bd13e167dc210243
Wesley Isaac <wesly972@hotmail.com>
2016-12-07 18:00:28 +0100

    accessibilite de la page admin uniquement aux admin

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -2,14 +2,16 @@
 
 
 @section('content')
-
+    @if(Auth::check() && Auth::user()->isAdmin)
 
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading"> Bievenue Admin , voici tous les articles et Evènements que les membres ont posté </div>
+                    <div class="panel-heading"> Bievenue Admin , voici tous les articles et Evènements que les membres ont postés </div>
                     <div class="panel-body">
+
+
 
 
                         @foreach($posts as $post)
@@ -25,7 +27,11 @@
 
 
 
+
                         {{ $posts->links()   }}
+@else  VOUS NETES PAS ADMINISTRATEUR DU SITE
+
+                        @endif
                     </div>
                 </div>
             </div>
